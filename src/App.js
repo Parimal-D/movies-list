307afdb94817a27ecd2645c951544b78c66d1e76
Parimal <itsmeparimal.d@gmail.com>
2026-08-17 16:24:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Header from "./components/header/Header";
+import Movies from "./components/movies/Movies";
+import Loader from "./components/loader/Loader";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Movies />
+    </>
+  );
+}
+
+export default App;
---
+++ src/apis/apis.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+const baseURL = process.env.REACT_APP_API_URL;
+const searchUrl = process.env.REACT_APP_SEARCH_URL;
+
+export async function getMoviesAPI() {
+  const response = await axios.get(baseURL);
+  console.log(response);
+  return response;
+}
+
+export async function getMoviesSearchAPI(searchKey) {
+  let searchValue = searchUrl + searchKey;
+  const response = await axios.get(searchValue);
+  return response;
+}
